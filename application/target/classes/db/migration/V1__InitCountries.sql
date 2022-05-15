b47b7d05028960fa73dec0b62b07914efaa94c3b
szerg97 <szerg97@gmail.com>
2022-05-15 22:02:35 +0200

    target folders cleared

--- application/target/classes/db/migration/V1__InitCountries.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE countries(
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    country_code VARCHAR(2),
-    name VARCHAR (128),
-    inserted DATETIME,
-    last_modified DATETIME,
-    visible BIT,
-    UNIQUE (country_code)
-);
--- application/target/classes/db/migration/V4__InitOpeningHours.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE opening_hours(
-        id BIGINT PRIMARY KEY AUTO_INCREMENT,
-        weekdays VARCHAR (16),
-        saturday VARCHAR (16),
-        sunday VARCHAR (16),
-        inserted DATETIME,
-        last_modified DATETIME,
-        visible BIT
-);
